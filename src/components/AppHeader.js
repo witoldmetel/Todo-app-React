6e946384e41a7af2356d597f60caa0c326bda88f
Managersky <witoldmetel@gmail.com>
2018-04-03 18:41:28 +0200

    Add searcher

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -5,10 +5,14 @@ class AppHeader extends Component {
         return (
                 <header className="ui fixed menu">
                     <nav className="ui container">
-                        <a href="#" className="header item"><img className="logo" src="http://taylorehat.com/images/todo-cloud-hero.png"/> Todo Application</a>
+                        <a href="#" className="header item"><img className="logo" src="https://api.adorable.io/avatars/55/TodoApp.png"/> Funny Todo App</a>
                         <div class="ui action input">
                             <input type="text" placeholder="Add new task"></input>
                             <button class="ui button">Add Task</button>
+                        </div>
+                        <div class="ui icon input">
+                                <input onInput={this.filterUsers.bind(this)} placeholder="Search task..."></input>
+                                <i class="search icon"></i>
                         </div>
                     </nav>
                 </header>

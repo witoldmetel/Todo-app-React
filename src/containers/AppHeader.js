4b99e124ba2eff0d531e6a51a63deea96efb7db5
Managersky <witoldmetel@gmail.com>
2018-04-13 14:31:50 +0200

    update

--- src/containers/AppHeader.js
+++ src/containers/AppHeader.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import TodoTextInput from '../components/TodoTextInput';
 import { addTask } from '../actions';
 
-export const AppHeader = ({ addTask }) => (
+export const AppHeader = ({ dispatch }) => (
     <header className="ui fixed menu">
         <nav className="ui container">
             <a href="#" className="header item"><img className="logo" src="https://api.adorable.io/avatars/55/TodoApp.png" /> Funny Todo App</a>
-            <TodoTextInput newTask onSave={(text) => {
-                if (text.length !== 0) {
-                    addTask(text)
-                }
-            }} placeholder="Add new task" />
+            <div className="ui action input">
+                <input placeholder="Add new task" ref="task"></input>
+                <button className="ui button" onClick={(e) => {
+                    if (!input.value.trim()) {
+                        return
+                    }
+                    dispatch(addTask(input.value))
+                    input.value = ''
+                    }}>Add Task</button>
+            </div>
             <div className="ui icon input">
                 <input type="text" placeholder="Search task..."></input>
                 <i className="search icon"></i>
@@ -21,8 +24,4 @@ export const AppHeader = ({ addTask }) => (
     </header>
 )
 
-AppHeader.PropTypes = {
-    addTask: PropTypes.func.isRequired
-}
-
-export default connect(null, { addTask })(AppHeader)
+export default connect()(AppHeader)

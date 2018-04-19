45f6fcdbc04f0077db794e5dfc70ff7cf7c12a69
Managersky <witoldmetel@gmail.com>
2018-04-19 14:43:26 +0200

    working static todo list

--- src/containers/TodoList.js
+++ src/containers/TodoList.js
@@ -4,15 +4,19 @@ import { connect } from 'react-redux';
 import TodoTask from './TodoTask';
 
 class TodoList extends React.Component {
+    renderList() {
+        return this.props.todos.map((todo) => {
+            return (
+                <TodoTask key={todo.taskNumber} randomFace={todo.taskNumber} taskNumber={todo.taskNumber} taskDescription={todo.taskDescription} />
+            );
+        });
+    }
+
     render() {
         return (
             <main className="ui main text container">
                 <ul className="ui relaxed divided list selection">
-                    {
-                        this.props.todos.map((todo) => {
-                            <TodoTask key={todo.taskNumber} randomFace={todo.taskNumber} taskNumber={todo.taskNumber} taskDescription={todo.taskDescription} />
-                        })
-                    }
+                    {this.renderList()}
                 </ul>
             </main>
         )

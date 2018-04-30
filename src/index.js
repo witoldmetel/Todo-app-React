a0c0e3fdcf91967377c84df3a0d713d513c38678
Managersky <witoldmetel@gmail.com>
2018-04-30 10:56:27 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk'; // middleware to dispatch functions instead of action objects and the middleware executes the function
+// import createLogger from "redux-logger"; //provide useful log message in browser console
 
 import App from './components/App';
 import reducers from './reducers';
 import './index.css';
 
-const store = createStore(reducers);
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+// const store = createStore(reducers);
 
 ReactDOM.render(
     <Provider store={store}>

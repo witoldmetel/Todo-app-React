a92bf27cbd10055f81111d795224196e23d0ee13
witoldmetel <witoldmetel@gmail.com>
2020-05-23 19:17:43 +0200

    mock up sign components

--- src/components/SignInComponent/SignInComponent.tsx
+++ src/components/SignInComponent/SignInComponent.tsx
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 
 export default class SignInComponent extends Component {
   public render() {
-    return <div>SignInComponent</div>;
+    return (
+      <form className="ui form">
+        <div className="field">
+          <label>Username</label>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div className="field">
+          <label>Password</label>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <button className="ui button" type="submit">
+          Login
+        </button>
+      </form>
+    );
   }
 }
--- src/components/SignUpComponent/SignUpComponent.tsx
+++ src/components/SignUpComponent/SignUpComponent.tsx
@@ -2,6 +2,24 @@ import React, { Component } from 'react';
 
 export default class SignUpComponent extends Component {
   public render() {
-    return <div>SignUpComponent</div>;
+    return (
+      <form className="ui form">
+        <div className="field">
+          <label>Username</label>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div className="field">
+          <label>E-mail</label>
+          <input type="email" placeholder="joe@schmoe.com" />
+        </div>
+        <div className="field">
+          <label>Password</label>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <button className="ui button" type="submit">
+          Submit
+        </button>
+      </form>
+    );
   }
 }

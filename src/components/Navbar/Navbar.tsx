12dd5dcc42783f5ab969ca4f481e51a2f3862139
witoldmetel <witoldmetel@gmail.com>
2020-09-07 16:47:59 +0200

    add first unit tests

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,14 +6,12 @@ import Links from './Links';
 import './Navbar.scss';
 
 export default class Navbar extends React.Component {
-  private logo = require('../../../public/assets/logo.png');
-
   public render() {
     return (
       <header className="ui menu">
         <nav className="ui container">
           <Link to="/" className="header item">
-            <img className="logo" src={this.logo} />
+            <span className="logo" />
             <p>Fire Jira</p>
           </Link>
           <Links {...this.props} />

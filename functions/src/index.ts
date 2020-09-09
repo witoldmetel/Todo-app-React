3668f0225f852ce749877995ecc039acfa10bee7
witoldmetel <witoldmetel@gmail.com>
2020-09-09 17:42:21 +0200

    add new fnc for updating

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,24 +4,43 @@ import * as admin from 'firebase-admin';
 admin.initializeApp(functions.config().firebase);
 
 const createNotification = (notification: any) => {
-  return admin
-    .firestore()
-    .collection('notifications')
-    .add(notification)
-    .then((doc) => console.log(doc));
+  return admin.firestore().collection('notifications').add(notification);
 };
 
+/**
+ * Project Functions
+ */
 export const projectCreated = functions.firestore.document('projects/{projectId}').onCreate((doc) => {
   const project = doc.data();
   const notification = {
-    content: 'Added a new project',
-    user: `${project.author}`,
+    content: 'create a new project',
+    user: project.author,
     time: admin.firestore.FieldValue.serverTimestamp(),
   };
 
   return createNotification(notification);
 });
 
+export const projectUpdate = functions.firestore.document('projects/{projectId}').onUpdate((change) => {
+  const before = change.before.data();
+  const after = change.after.data();
+
+  if (before.members.length !== after.members.length) {
+    const notification = {
+      content: `change crew members in ${before.projectName} project`,
+      user: before.author,
+      time: admin.firestore.FieldValue.serverTimestamp(),
+    };
+
+    return createNotification(notification);
+  }
+
+  return null;
+});
+
+/**
+ * User Functions
+ */
 export const userJoined = functions.auth.user().onCreate((user) => {
   return admin
     .firestore()
@@ -31,8 +50,8 @@ export const userJoined = functions.auth.user().onCreate((user) => {
     .then((doc) => {
       const newUser = doc.data();
       const notification = {
-        content: 'Joined new user',
-        user: `${newUser?.username}`,
+        content: 'joined to Fire Jira',
+        user: newUser?.username,
         time: admin.firestore.FieldValue.serverTimestamp(),
       };
 

f29202bbe7c4ad7a07ca7791a72c04447e9622a3
witoldmetel <witoldmetel@gmail.com>
2020-09-20 12:18:48 +0200

    Prepare 404 page

--- src/components/UnknownPage/UnknownPage.tsx
+++ src/components/UnknownPage/UnknownPage.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './UnknownPage.scss';
 
 export class UnknownPage extends React.Component {
   render() {
-    return <div className="unknown-page">Unknown Page</div>;
+    return (
+      <div className="unknown-page">
+        <div className="container">
+          <div className="logo-container">
+            <p>4</p>
+            <span className="logo" />
+            <p>4</p>
+          </div>
+          <p>Nice try! That was one in a million.</p>
+          <p>
+            Let&#39;s get you <Link to="/">back</Link>
+          </p>
+        </div>
+      </div>
+    );
   }
 }

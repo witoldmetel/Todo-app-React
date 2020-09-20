f8cb746abf7846a01b2c63b349dba3ca19a0a751
Witold Mętel <31034370+witoldmetel@users.noreply.github.com>
2020-09-20 12:52:55 +0200

    Spike: Implement 404 page (#39)

---
+++ src/components/UnknownPage/UnknownPage.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './UnknownPage.scss';
+
+export class UnknownPage extends React.Component {
+  render() {
+    return (
+      <div className="unknown-page">
+        <div className="container">
+          <div className="logo-container">
+            <p>4</p>
+            <span className="logo" />
+            <p>4</p>
+          </div>
+          <p>Nice try! That was one in a million.</p>
+          <p>
+            Let&#39;s go <Link to="/">back</Link>
+          </p>
+        </div>
+      </div>
+    );
+  }
+}

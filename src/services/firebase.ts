e248ab654614871a103a81bc1b1f819f2b28556f
witoldmetel <witoldmetel@gmail.com>
2020-10-14 17:16:53 +0200

    fix

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,7 +3,20 @@ import 'firebase/auth';
 
 import { FIREBASE_CONFIG } from './firebaseConfig';
 
+const firebaseConfig = FIREBASE_CONFIG
+  ? FIREBASE_CONFIG
+  : {
+      apiKey: 'dummy_value',
+      authDomain: 'dummy_value',
+      databaseURL: 'https://fire-jira.firebaseio.com',
+      projectId: 'fire-jira',
+      storageBucket: 'fire-jira.appspot.com',
+      messagingSenderId: 'dummy_value',
+      appId: 'dummy_value',
+      measurementId: 'dummy_value',
+    };
+
 // Initialize firebase instance
-firebase.initializeApp(FIREBASE_CONFIG);
+firebase.initializeApp(firebaseConfig);
 
 export default firebase;

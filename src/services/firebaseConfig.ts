a4f59946e1786bf6d6e02e60529cff839b4185a1
witoldmetel <witoldmetel@gmail.com>
2021-03-04 17:44:56 +0100

    Bugfix: Hide firebase config

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -1,10 +1,10 @@
 export const FIREBASE_CONFIG = {
-  apiKey: 'AIzaSyA-jkwh2pE93mflkaeqaxtu5ODC98Io4DQ',
-  authDomain: 'fire-jira.firebaseapp.com',
-  databaseURL: 'https://fire-jira.firebaseio.com',
-  projectId: 'fire-jira',
-  storageBucket: 'fire-jira.appspot.com',
-  messagingSenderId: '925649553379',
-  appId: '1:925649553379:web:00226c76f9912a1d7502d6',
-  measurementId: 'G-4K5M4GYG7R'
+  apiKey: 'API_KEY',
+  authDomain: 'PROJECT_ID.firebaseapp.com',
+  databaseURL: 'https://PROJECT_ID.firebaseio.com',
+  projectId: 'PROJECT_ID',
+  storageBucket: 'PROJECT_ID.appspot.com',
+  messagingSenderId: 'SENDER_ID',
+  appId: 'APP_ID',
+  measurementId: 'G-MEASUREMENT_ID'
 };
